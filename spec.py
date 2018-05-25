7aa7c9978f2004744cd4685be6038162c383e09b
Yaroslav Halchenko <debian@onerussian.com>
2018-05-24 21:50:37 -0400

    Regenerated ANTS gear with ordering of options prioritizing non-optional
    Closes #2

--- spec.py
+++ spec.py
@@ -78,6 +78,8 @@ spec = {
                         ),
                     }
                 },
+                # TODO: handle TraitCompound
+                #"Registration": {},
             },
             "segmentation": {
                 "CorticalThickness": {},
